1ba62ab1970003c4ad12634ad63b602cafeda581
harlan <hello@harlankellaway.com>
2018-11-21 13:38:44 -0500

    Created Xcode framework project

---
+++ AsyncTaskPerformer/AsyncTaskPerformer/AsyncTaskPerformer.h
@@ -0,0 +1,19 @@
+//
+//  AsyncTaskPerformer.h
+//  AsyncTaskPerformer
+//
+//  Created by Harlan Kellaway on 11/21/18.
+//  Copyright © 2018 Harlan Kellaway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AsyncTaskPerformer.
+FOUNDATION_EXPORT double AsyncTaskPerformerVersionNumber;
+
+//! Project version string for AsyncTaskPerformer.
+FOUNDATION_EXPORT const unsigned char AsyncTaskPerformerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsyncTaskPerformer/PublicHeader.h>
+
+
